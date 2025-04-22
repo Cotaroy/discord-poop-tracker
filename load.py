b92c54af77bfbc6708a918065e5ecb3592f8bf7c
Vvoltaic <twinflame265@gmail.com>
2025-04-22 01:13:13 -0400

    Update load.py

--- load.py
+++ load.py
@@ -1,7 +1,33 @@
 
+import json
+from datetime import datetime
+
+from user import User
+from poops import Poops
 from const import USER_DATA_FOLDER
 
 
-def load_user(id: str):
+def load_user(id: str) -> User:
     """load user data from USER_DATA_FOLDER/id.json"""
-    with open()
+    file_path = f'{USER_DATA_FOLDER}{id}.json'
+    with open(file_path, 'r') as f:
+        user_data = json.load(file_path, f)
+
+    name = user_data['name']
+
+    total = user_data['poops']['total']
+
+    log = {}
+    for date in user_data['poops']:
+        if date != 'total':
+            log[date] = user_data['poops'][date]
+
+    today = str(datetime.today().date())
+    if today in log:
+        today = log[today]
+    else:
+        today = 0
+
+    poops = Poops(today, total, log)
+
+    return User(id, name, poops)
